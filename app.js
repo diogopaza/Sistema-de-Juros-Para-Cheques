95058093d9f9556df909119399477e7ebb1b4832
diogopaza <diogopazacvel@gmail.com>
2022-02-27 19:52:29 -0300

    sending to heroku

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+var express =require('express');
+
+var app = express();
+var porta = process.env.PORT || 2020;
+//app.set('port', porta);
+
+app.get('/', function(req,res){
+    res.send("Calculadora de juros");
+})
+
+app.listen(porta, function(){
+    console.log("Sistema de juros rodando");
+});
+
 let juro = 1.2;
 let ValorTotal =0;
 for(i=1;i<97;i++){
